9f0fc004798a7626ed91a81b2d3a4475050b5b11
Shun Ueda <me@shu.nu>
2024-10-08 18:29:45 -0400

    fix(prepare): fix vercel prepare script

--- scripts/prepare/index.ts
+++ scripts/prepare/index.ts
@@ -3,31 +3,28 @@ import { EOL } from 'node:os'
 import { parseEnv } from 'node:util'
 import { $ } from 'zx'
 
-await $`vercel env pull`
-
 const envFile = '.env.local'
 
-if (!existsSync(envFile)) {
-  console.error(`No ${envFile} file found!`)
-  process.exit(1)
-}
+const env = existsSync(envFile)
+  ? parseEnv(readFileSync(envFile).toString())
+  : process.env // vercel
 
-const content = readFileSync(envFile).toString()
-const env = parseEnv(content)
-const dts = `
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      ${Object.keys(env)
-        .map(key => `${key}: string`)
-        .join(`${EOL}      `)}
-    }
-  }
-}
-
-export {}
-`
-
-writeFileSync('environment.d.ts', dts)
+writeFileSync('environment.d.ts', createDts(env))
 
 await $`lefthook install`
+
+function createDts(env: object) {
+  return `
+    declare global {
+      namespace NodeJS {
+        interface ProcessEnv {
+          ${Object.keys(env)
+            .map(key => `${key}: string`)
+            .join(`${EOL}      `)}
+        }
+      }
+    }
+    
+    export {}
+`
+}
