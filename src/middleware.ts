e0d8900f7dfbfd427f577ead7dd6d0bfb4afc0cb
Shun Ueda <me@shu.nu>
2024-10-18 16:12:35 -0400

    refactor(middleware): cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,39 @@
-import AcceptLanguage from 'accept-language'
-import {
-  type MiddlewareConfig,
-  type NextRequest,
-  NextResponse
-} from 'next/server'
+import { pick } from 'accept-language-parser'
+import { type NextRequest, NextResponse } from 'next/server'
+import { Cookie } from '#lib/cookie'
+import { Header } from '#lib/header'
 import { Lang, langs } from '#lib/i18n'
-
-AcceptLanguage.languages(langs as string[])
-
-export const config: MiddlewareConfig = {
-  matcher: ['/', '/blog/:path*']
-}
-
-const cookieLangKey = 'lang'
-const acceptLanguageHeader = 'accept-language'
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
-  const pathnameLang = langs.find(lang => pathname.startsWith(`/${lang}`))
-  if (pathnameLang) {
+  const pathlang = langs.find(lang => pathname.startsWith(`/${lang}`))
+  if (pathlang) {
     const response = NextResponse.next()
-    response.cookies.set(cookieLangKey, pathnameLang)
+    response.cookies.set(Cookie.LANG, pathlang)
     return response
   }
-  const lang = getUserLang(request)
+  const lang = getLang(request)
   const url = new URL(`/${lang}${pathname}`, request.url)
   return NextResponse.redirect(url)
 }
 
-function getUserLang(request: NextRequest) {
+function getLang(request: NextRequest) {
   return (
-    request.cookies.get(cookieLangKey)?.value ??
-    AcceptLanguage.get(request.headers.get(acceptLanguageHeader)) ??
+    request.cookies.get(Cookie.LANG)?.value ||
+    pick(langs as Lang[], request.headers.get(Header.ACCEPT_LANGUAGE) || '') ||
     Lang.EN
   )
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+  ]
 }
