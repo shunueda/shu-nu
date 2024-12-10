8481da4afd7f9efe131d3bd06282f86549f30dbc
Shun Ueda <me@shu.nu>
2024-12-09 23:03:47 -0500

    revert(package.json): add homepage

--- src/app/[lang]/resume/page.tsx
+++ src/app/[lang]/resume/page.tsx
@@ -2,7 +2,7 @@ import { capitalCase } from 'change-case'
 import { FileTextIcon } from 'lucide-react'
 import Link from 'next/link'
 import resume from '#assets/resume.json'
-import { Button } from '../../../components/ui/button'
+import { Button } from '#components/ui/button'
 
 export default function Page() {
   return (
