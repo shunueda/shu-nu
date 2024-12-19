5c12fa357a6d256e557af16ab4ab68eb73a11698
Shun Ueda <me@shu.nu>
2024-12-18 21:00:59 -0500

    fix(resume): make script failable

--- scripts/pdf/index.ts
+++ scripts/pdf/index.ts
@@ -4,34 +4,30 @@ import { format, join } from 'node:path'
 import resume from '#assets/resume.json'
 import { generate } from './generate'
 
-try {
-  const outfile = 'Shun_Ueda_Resume'
-  const outdir = 'public'
+const outfile = 'Shun_Ueda_Resume'
+const outdir = 'public'
 
-  const templateFile = format({
-    dir: 'src/assets',
-    name: 'template.tex'
-  })
-  const template = await readFile(templateFile, 'utf-8')
+const templateFile = format({
+  dir: 'src/assets',
+  name: 'template.tex'
+})
+const template = await readFile(templateFile, 'utf-8')
 
-  const texfile = format({
-    dir: outdir,
-    name: outfile,
-    ext: 'tex'
-  })
+const texfile = format({
+  dir: outdir,
+  name: outfile,
+  ext: 'tex'
+})
 
-  await writeFile(texfile, await generate(resume, template))
+await writeFile(texfile, await generate(resume, template))
 
-  execSync(`pdflatex ${texfile}`)
+execSync(`pdflatex ${texfile}`)
 
-  const pdffile = format({
-    name: outfile,
-    ext: 'pdf'
-  })
+const pdffile = format({
+  name: outfile,
+  ext: 'pdf'
+})
 
-  await rename(pdffile, join(outdir, pdffile))
+await rename(pdffile, join(outdir, pdffile))
 
-  execSync(`latexmk -c -f ${texfile}`)
-} catch (_: unknown) {
-  console.log('Failed to generate resume - you must be Vercel.')
-}
+execSync(`latexmk -c -f ${texfile}`)
