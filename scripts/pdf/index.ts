9da971e57f77dcb407444497ff8d433ae50dd5c2
Shun Ueda <me@shu.nu>
2025-01-16 17:03:52 -0500

    fix(env): make env optional for pdf generation

--- scripts/pdf/index.ts
+++ scripts/pdf/index.ts
@@ -1,6 +1,5 @@
 import { writeFile } from 'node:fs/promises'
 import { google } from '@ai-sdk/google'
-import { pipe } from 'effect'
 import resume from '~/assets/resume.json' with { type: 'json' }
 import { File } from '#lib/file'
 import { compile } from './compile'
@@ -8,13 +7,11 @@ import { generate } from './generate'
 
 const out = `public/${File.RESUME}.pdf`
 
-await pipe(
-  await generate({
-    resume,
-    template: 'assets/template.tex',
-    model: google('gemini-1.5-flash')
-  }),
-  compile,
-  async it => Buffer.from(await it),
-  async it => writeFile(out, await it)
-)
+const generated = await generate({
+  resume,
+  template: 'assets/template.tex',
+  model: google('gemini-1.5-flash')
+})
+const compiled = await compile(generated)
+const buffer = Buffer.from(compiled)
+await writeFile(out, buffer)
