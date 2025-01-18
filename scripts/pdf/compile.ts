b15cf61938978c529932d13a6d57c24554ae3ea6
Shun Ueda <me@shu.nu>
2025-01-17 19:22:11 -0500

    refactor(compile): cleanup

--- scripts/pdf/compile.ts
+++ scripts/pdf/compile.ts
@@ -1,20 +1,20 @@
+/**
+ * Compiles a LaTeX document to a PDF using TeXLive.net server.
+ *
+ * {@link https://davidcarlisle.github.io/latexcgi/}
+ */
 const endpoint = 'https://texlive.net/cgi-bin/latexcgi'
 
 const body = new FormData()
 body.append('filename[]', 'document.tex') // The server expects this exact name
 body.append('return', 'pdf')
 
-/**
- * Compiles a LaTeX document to a PDF using texlive.net (latexcgi).
- *
- * {@link https://davidcarlisle.github.io/latexcgi/}
- */
 export async function compile(content: string) {
   body.append('filecontents[]', content)
-  const response = await fetch(endpoint, {
+  return fetch(endpoint, {
     method: 'POST',
     body
   })
-  const arrayBuffer = await response.arrayBuffer()
-  return Buffer.from(arrayBuffer)
+    .then(it => it.arrayBuffer())
+    .then(Buffer.from)
 }
