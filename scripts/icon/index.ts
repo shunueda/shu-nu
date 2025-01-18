36215972907a952a35df852c500e2e7c2e69348e
Shun Ueda <me@shu.nu>
2025-01-18 15:12:01 -0500

    refactor: cleanup

--- scripts/icon/index.ts
+++ scripts/icon/index.ts
@@ -1,19 +1,26 @@
 import { mkdir, writeFile } from 'node:fs/promises'
+import React, { createElement } from 'react'
+import satori from 'satori'
 import sharp from 'sharp'
 import toIco from 'to-ico'
-import { generate } from './generate'
+import { Icon } from '#components/icon'
+
+// polyfill to use JSX in node
+globalThis.React = React
 
 const out = 'public'
 const sizes = [16, 24, 32, 48, 64, 128, 256] as const
 const faviconPath = 'src/app/favicon.ico'
 
-await mkdir(out, {
-  recursive: true
-})
+await mkdir(out)
 
 const pngs = await Promise.all(
   sizes.map(async size => {
-    const svg = await generate(size)
+    const svg = await satori(createElement(Icon), {
+      width: size,
+      height: size,
+      fonts: []
+    })
     const buffer = Buffer.from(svg)
     const png = await sharp(buffer).png().toBuffer()
     // save icon files
