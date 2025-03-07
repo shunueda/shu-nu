63c69ed09e68f1c4a5182f8b0cc54f7d600d2096
Shun Ueda <me@shu.nu>
2025-03-07 15:40:02 -0500

    feat(analytics): add link analytics

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,3 +1,4 @@
+import { track } from '@vercel/analytics'
 import type { Route } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -31,7 +32,13 @@ export async function Nav(): Promise<ReactElement> {
         <Image src={icon} alt={icon} fill />
       </div>
       {items.map(({ href, label, target }) => (
-        <Link className='text-gray-700' href={href} target={target} key={label}>
+        <Link
+          className='text-gray-700'
+          href={href}
+          target={target}
+          key={label}
+          onClick={(): void => track(href)}
+        >
           {label}
         </Link>
       ))}
